22825e374c365d5b4b82181d8ff6fd5a487793dd
jglorange <jglorange@gmail.com>
2020-07-11 16:28:18 +0000

    stöff

--- src/pageComponents/Documentaries/index.js
+++ src/pageComponents/Documentaries/index.js
@@ -1,33 +1,18 @@
 import React, { useState, useEffect, useContext } from "react"
-import { collectionData } from "rxfire/firestore"
 import { Grid } from "../Sarpur/styled"
 import Movie from "./movie"
 import { RootContext } from "../../context/main"
 
-const Documentaries = ({ year }) => {
-  const { firestore } = useContext(RootContext)
-
-  const [movies, setMovies] = useState([])
-
-  useEffect(() => {
-    if (firestore) {
-      let docRef = firestore.collection("movies")
-      const subscription = collectionData(docRef).subscribe(movies => setMovies(movies))
-      return () => subscription.unsubscribe()
-    }
-  }, [firestore, year])
-
-  if (!firestore) {
-    return null
-  } else {
-    return (
+const Documentaries = ({ docs }) => {
+  return (
+    docs && (
       <Grid>
-        {movies.map((movie, index) => (
+        {docs.nodes.map((movie, index) => (
           <Movie movie={movie} key={index}></Movie>
         ))}
       </Grid>
     )
-  }
+  )
 }
 
 export default Documentaries
--- src/pages/heimildamyndir.js
+++ src/pages/heimildamyndir.js
@@ -1,22 +1,84 @@
-import React from 'react'
-import Header from '../layouts/Header'
-import Template from '../reusableComponents/SarpurTemplate'
-import Documentaries from '../pageComponents/Documentaries'
+import React from "react"
+import Header from "../layouts/Header"
+import Footer from "../layouts/Footer"
+import Template from "../reusableComponents/SarpurTemplate"
+import Documentaries from "../pageComponents/Documentaries"
+import { graphql } from "gatsby"
 
 const TITLES = {
-  is: 'Heimildamyndir 2020',
-  en: 'Documentaries 2020'
+  is: "Heimildamyndir 2020",
+  en: "Documentaries 2020",
 }
 
-const Heimildamyndir = () => {
+const Heimildamyndir = ({
+  data: { frumsyningar: docs, verk_i_vinnslu: wips },
+}) => {
   return (
     <>
       <Header mode='red' />
       <Template title={TITLES}>
-        <Documentaries />
+        <Documentaries docs={docs} />
       </Template>
+      <Footer />
     </>
   )
 }
+
+export const query = graphql`
+  {
+    frumsyningar: allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fileAbsolutePath: { regex: "/frumsyning/" } }
+    ) {
+      nodes {
+        html
+        frontmatter {
+          title
+          director
+          producer
+          production_company
+          editing
+          filming
+          composer
+          sound_design
+          length_in_min
+          Trailer
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+
+    verk_i_vinnslu: allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fileAbsolutePath: { regex: "/verk-i-vinnslu/" } }
+    ) {
+      nodes {
+        html
+        frontmatter {
+          title
+          director
+          producer
+          length_in_min
+          Trailer
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Heimildamyndir
