03ddd39e1143a25cbc59f3aa64458a17bfc69f8f
Jón Gabríel Lorange <jonl17@ru.is>
2022-02-20 15:15:00 +0000

    remove old shut

--- src/reusableComponents/Logo/index.js
+++ src/reusableComponents/Logo/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Container } from './styled'
-import Logo from './svg'
 
 export default ({ footer, color }) => {
   return (
     <Container footer={footer ? 'true' : 'false'} to='/'>
-      <Logo color={color}></Logo>
       bobsy
     </Container>
   )
